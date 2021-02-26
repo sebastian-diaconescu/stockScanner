302457f30b7a2316a204de1ef27bc8466cbd2db9
sebastian-diaconescu <s3bastian.diaconescu@gmail.com>
2021-02-26 18:07:11 +0000

    fix

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-<<<<<<< HEAD
 
 @app.route('/')
 def index():
@@ -10,28 +9,3 @@ def index():
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
-=======
-from flask import Flask
-from flask_bootstrap import Bootstrap
-from flask import request, render_template
-from dependency_injector.wiring import inject, Provide
-
-from containers import Container
-
-
-@inject
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-def create_app() -> Flask:
-    container = Container()    
-    container.wire(modules=[views])
-    app = Flask(__name__)
-    app.container = container
-    app.add_url_rule('/', 'index', views.index)
-
-    bootstrap = Bootstrap()
-    bootstrap.init_app(app)
-
-    return app
->>>>>>> 119ca400c34a53a9defac0a0e21b964d1dff0b0c
--- containers.py
+++
@@ -1,6 +0,0 @@
-from dependency_injector import containers, providers
-
-
-class Container(containers.DeclarativeContainer):
-    config = providers.Configuration()
-    #add members here
