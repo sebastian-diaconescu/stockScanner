a96666cd7f36207528fdd8b61e6c552d2fa006d9
Sebastian Diaconescu <s3bastian.diacoenscu@gmail.com>
2021-02-26 20:34:49 +0200

    merged

--- app.py
+++ app.py
@@ -2,28 +2,13 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-<<<<<<< HEAD
-#from .containers import Container
-from . import views
-=======
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
->>>>>>> c8380942ebe081cd5a73fdfa6798dc0bd5989157
 
 @app.route('/scan/')
 def scanFinvizTicker():
     return request.query_string
-
-<<<<<<< HEAD
-def create_app() -> Flask:
-    #container = Container()
-    #container.wire(modules=[views])
-    app = Flask(__name__)
-    #app.container = container
-    app.add_url_rule('/', 'index', views.index)
-=======
->>>>>>> c8380942ebe081cd5a73fdfa6798dc0bd5989157
 
 
 if __name__ == '__main__':
