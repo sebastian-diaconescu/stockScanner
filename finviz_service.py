bc6d03b9309015d8d709ae914bc606380f211670
sebastian-diaconescu <s3bastian.diaconescu@gmail.com>
2021-02-27 19:26:05 +0000

    fixed typos

--- finviz_service.py
+++ finviz_service.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 class FinVizHelper:
     def getStockPE(self, ticker):
+        #TODO: try to optimize calls 
         stock = finvizfinance(ticker)
         fundament = stock.TickerFundament()
         return fundament
@@ -12,8 +13,9 @@ class FinVizHelper:
         #url = "https://finviz.com/quote.ashx?t="
         #page = url + ticker
         pageURl = "https://finviz.com/quote.ashx?t=gme"
+        #TODO: use random list of useragents 
         headers={'User-Agent': 'Mozilla/5.0'}
-        
+        #TODO: move this into a method to optimize number of calls to finviz
         response = requests.get(pageURl, headers=headers)
         if (response.status_code != 200):
             return "no response from page " + response.status_code
@@ -25,7 +27,7 @@ class FinVizHelper:
             anArticle = newsTable[i].text
             allArticles.append(anArticle)
 
-        return sallArticles
+        return allArticles
         
         
         
