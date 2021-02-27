85f5830ce17e1e14e1bf265a82dfdfd72b828061
sebastian-diaconescu <s3bastian.diaconescu@gmail.com>
2021-02-27 14:40:36 +0000

    fixed crash

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def loadRevolut():
 
     return "Refresh complete"
 
-@app.route('/load-sentiment/', methods['POST'])
+@app.route('/load-sentiment/', methods=['POST'])
 def loadSentiment():
     ticker = request.args.get('ticker')
     db = DBConnection()
