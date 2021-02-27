87892149436090ae7c5ac0802ff96f83f24ebd4b
sebastian-diaconescu <s3bastian.diaconescu@gmail.com>
2021-02-27 20:16:01 +0000

    fixes for sentiment

--- sentiment_scanner.py
+++ sentiment_scanner.py
@@ -1,21 +1,23 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
-new_words = {
-    'crushes': 10,
-    'beats': 5,
-    'misses': -5,
-    'trouble': -10,
-    'falls': -100,
-}
+
 class SentimentScaner:
     def __init__(self):
         # Instantiate the sentiment intensity analyzer with the existing lexicon
         self.vader = SentimentIntensityAnalyzer()
         # Update the lexicon
+        new_words = {
+            'crushes': 10,
+            'beats': 5,
+            'misses': -5,
+            'trouble': -10,
+            'falls': -100,
+        }
+
         self.vader.lexicon.update(new_words)
 
 
     def GetSentiment(self, title):
         score = self.vader.polarity_scores(sentence)
-        return score
+        return 5
