cb00ba6987107d8cd41390dfbfea3d9f5c3a9aca
sebastian-diaconescu <s3bastian.diaconescu@gmail.com>
2021-02-27 08:44:05 +0000

    fixes for db

--- database.py
+++ database.py
@@ -10,6 +10,7 @@ class DBConnection:
         self.engine = create_engine('postgres://dnjeujepwaungq:878cd5627953bf0d4ae99139a3733d9e07bf12429231ece3fc1ef23812468f9f@ec2-63-34-97-163.eu-west-1.compute.amazonaws.com:5432/dfuhv8ajipjhcs')
       
     def createTables(self):  
+        self.engine.connect()
         metadata = MetaData(self.engine)                 
         Table("fundamental", metadata,
             Column('Id', Integer, primary_key=True, nullable=False), 
@@ -20,6 +21,7 @@ class DBConnection:
         
 
     def InsertData(self, fundamentalData, ticker):
+        conn = self.engine.connect()
         conn.execute('INSERT INTO "fundamental" (name) '
              'VALUES (1, "test", "test")')
         trans.commit()
