424d366c1eadf443c15c7eecab0125e7d941cfe0
sebastian-diaconescu <s3bastian.diaconescu@gmail.com>
2021-03-05 19:58:19 +0000

    fixed return

--- database.py
+++ database.py
@@ -45,8 +45,10 @@ class DBConnection:
     def InsertHeadlineSentiment(self, date, headline, headline_hash, sentiment_score, ticker):
         conn = self.engine.connect()
 
-        return insert = "INSERT INTO headline_sentiment (date, headline, headline_hash, sentiment_score, ticker)" + 
+        insert = "INSERT INTO headline_sentiment (date, headline, headline_hash, sentiment_score, ticker)" + 
         "Values ('"+ str(date) + "', '" + headline + "', '" + headline_hash + "', '" + str(sentiment_score) + "', '" + ticker + "')"
+
+        return insert
 
         conn.execute(insert)
         conn.close()
