4f7f041e75af59759ce7125a1175739ba7e95e1d
sebastian-diaconescu <s3bastian.diaconescu@gmail.com>
2021-03-05 19:44:19 +0000

    to str for hash

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def loadSentiment():
         title = split[1]
         sentimentScore = sentimentScaner.GetSentiment(title)
         res = {"date":date, "title": title, "score": sentimentScore, "ticker":ticker }
-        hashVal = hashlib.md5(title.encode('utf-8')) 
+        hashVal = hashlib.md5(title.encode('utf-8')).hexdigest() 
 
         data = db.GetTitleByHash(hashVal)
         if (data == None):
